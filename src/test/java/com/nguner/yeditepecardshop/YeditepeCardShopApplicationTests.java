16f330e4fcc28cfabcb1319bceb276f47c48be5e
Nedim Eren Güner <nedim_gta@hotmail.com>
2025-06-08 16:46:37 +0300

    Configure MongoDB Atlas connection

---
+++ src/test/java/com/nguner/yeditepecardshop/YeditepeCardShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nguner.yeditepecardshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YeditepeCardShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
